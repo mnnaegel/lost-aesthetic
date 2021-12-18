876b675eea101ef0182a9dd7b251f243e94d5ecb
mnnaegel <86453692+mnnaegel@users.noreply.github.com>
2021-12-17 20:37:35 -0500

    Basic navbar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React from 'react'
 import './App.css'
-import Navbase from './components/NavBase.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button, Alert, Breadcrumb, Cart, Card } from 'react-bootstrap';
-
+import Navbase from './components/NavBase.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbase />
-        <Alert variant="success">This is a button!!!</Alert>
-        <Button>Test Button</Button>
-        <Breadcrumb>
-          <Breadcrumb.Item>Test</Breadcrumb.Item>
-          <Breadcrumb.Item>Test2</Breadcrumb.Item>
-          <Breadcrumb.Item active>Test3</Breadcrumb.Item>
-        </Breadcrumb>
-        <Card className="mb-3" style={{color:'black'}}>
-          <Card.Img src="https://picsum.photos/200/300"/>
-          <Card.Body>
-            <Card.Title>
-              Card Example
-            </Card.Title>
-            <Card.Text>
-              This is an example of bootstrap carsd
-            </Card.Text>
-            <Button variant="primary">Read More</Button>
-          </Card.Body>
-        </Card>
-      </header>
+    <div>
+      <Navbase />
     </div>
   );
 }
